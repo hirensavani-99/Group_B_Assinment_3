04e0d8f147d1899fd43b71a685215eb4c281447b
hiren <72260974+hirensavani-99@users.noreply.github.com>
2024-02-15 18:09:45 -0500

    set up for 3rd assinment

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "net/http"
+
+//Adding Items to storage
+func handleAddItem(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//Get list of Items from storage
+func handleGetItem(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+}
