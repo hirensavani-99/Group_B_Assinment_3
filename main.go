a46dc0b5543def12a18058e9c17a1038888f77de
aafaq-prog <hussainaafaq000@gmail.com>
2024-02-15 20:07:13 -0500

    defined struct and memory store

--- main.go
+++ main.go
@@ -2,6 +2,19 @@ package main
 
 import "net/http"
 
+// Item represents an item with ID, Name, Description, and Price
+type Item struct {
+	ID          string  `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+}
+
+// ItemStorage represents the in-memory storage for items
+type ItemStorage struct {
+	Items []Item
+}
+
 //Adding Items to storage
 func handleAddItem(w http.ResponseWriter, r *http.Request) {
 
